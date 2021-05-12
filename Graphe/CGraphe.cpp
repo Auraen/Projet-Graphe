206f65f48927a8a7080c1a8a9964fe64380c718c
Youssef696 <75251872+Youssef696@users.noreply.github.com>
2021-05-12 08:31:57 +0200

    Graphe

--- Graphe/CGraphe.cpp
+++ Graphe/CGraphe.cpp
@@ -28,3 +28,19 @@ void CGraphe::GRAmodifier_nb_arcs(int iNombre)
 {
 	iGRANb_arcs = iNombre;
 }
+
+void CGraphe::GRAsupprimer_sommet(CSommet SOMsommet)
+{
+	int iNombreArcs = SOMsommet.iSOMNb_entrant + SOMsommet.iSOMNb_sortant;
+	iGRANb_arcs =iGRANb_arcs - iNombreArcs;
+	iGRANb_sommets = iGRANb_sommets - 1;
+	CGraphe::GRAsupprimer_sommet(SOMsommet);
+
+}
+
+void CGraphe::GRAmodifier_sommet(int iNumero, CSommet SOMsommet)
+{
+	int iDiffrence = pGRASommets[iNumero].iSOMNb_entrant + pGRASommets[iNumero].iSOMNb_sortant - SOMsommet.iSOMNb_entrant - SOMsommet.SOMlire_nb_sortant;
+	iGRANb_arcs = iGRANb_arcs - iDifference;
+	pGRASommets[iNumero] = SOMsommet;
+}
