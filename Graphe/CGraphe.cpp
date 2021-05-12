ae7ef46f269f1550e7a3b9e4668943a96791a761
Auraen <aurane.martin@etu.univ-tours.fr>
2021-05-12 10:14:38 +0200

    Classe graph finie

--- Graphe/CGraphe.cpp
+++ Graphe/CGraphe.cpp
@@ -2,11 +2,43 @@
 
 CGraphe::CGraphe() {
 	iGRANb_sommets = 0;
-	pGRASommets = new CSommet[iGRANb_sommets];
+	pGRASommets = (CSommet**)realloc(pGRASommets, sizeof(CSommet)*iGRANb_sommets);
+}
+
+CGraphe::CGraphe(int iNbSommets, CSommet** pSommet) {
+	int iBoucle;
+	iGRANb_sommets = iNbSommets;
+	pGRASommets = (CSommet**)realloc(pGRASommets, sizeof(CSommet)*iGRANb_sommets);
+
+	for (iBoucle = 0; iBoucle < iGRANb_sommets; iBoucle++) {
+		pGRASommets[iBoucle] = new CSommet(*pSommet[iBoucle]);
+	}
 }
 
 CGraphe::~CGraphe() {
-	delete[] pGRASommets;
+	int iBoucle;
+	for (iBoucle = 0; iBoucle < iGRANb_sommets; iBoucle++)
+		delete(pGRASommets[iBoucle]);
+
+	free(pGRASommets);
+}
+
+void CGraphe::GRAajouter_sommet(CSommet* pSommet)
+{
+	iGRANb_sommets++;
+	pGRASommets = (CSommet**)realloc(pGRASommets, sizeof(CSommet)*iGRANb_sommets);
+
+	pGRASommets[iGRANb_sommets] = pSommet;
+}
+
+
+void CGraphe::GRAsupprimer_sommet(CSommet SOMsommet)
+{
+	int iNombreArcs = SOMsommet.iSOMNb_entrant + SOMsommet.iSOMNb_sortant;
+	iGRANb_arcs = iGRANb_arcs - iNombreArcs;
+	iGRANb_sommets = iGRANb_sommets - 1;
+	CGraphe::GRAsupprimer_sommet(SOMsommet);
+
 }
 
 int CGraphe::GRAlire_nb_sommet()
@@ -14,22 +46,33 @@ int CGraphe::GRAlire_nb_sommet()
 	return iGRANb_sommets;
 }
 
-void CGraphe::GRAmodifier_nb_sommet(int iNombre)
+void CGraphe::GRAajouter_arc(int iSommet_depart, int iSommet_arrivee)
 {
-	iGRANb_sommets = iNombre;
+	int iBoucle;
+	int iPosSomDep = -1;
+	int iPosSomArv = -1;
+
+	while ((iPosSomDep != -1 && iPosSomArv != -1) || iBoucle > iGRANb_sommets) {
+		if (iSommet_depart == pGRASommets[iBoucle]->SOMlire_numero_sommet)
+			iPosSomDep = iBoucle;
+
+		if (iSommet_arrivee == pGRASommets[iBoucle]->SOMlire_numero_sommet)
+			iPosSomArv = iBoucle;
+
+		iBoucle++;
+	}
+
+	if (iPosSomArv != -1 && iPosSomDep != -1) {
+		pGRASommets[iPosSomDep]->SOMajouter_arc_sortant(new CArc(iSommet_depart));
+		pGRASommets[iPosSomArv]->SOMajouter_arc_entrant(new CArc(iSommet_arrivee));
+	}
+	else {
+		void();
+		//throw exception: un des deux sommets n'existe pas
+	}
 }
 
-int CGraphe::GRAlire_nb_arcs()
-{
-	return iGRANb_arcs;
-}
-
-void CGraphe::GRAmodifier_nb_arcs(int iNombre)
-{
-	iGRANb_arcs = iNombre;
-}
-
-void CGraphe::GRAsupprimer_sommet(CSommet SOMsommet)
+/*void CGraphe::GRAsupprimer_sommet(CSommet SOMsommet)
 {
 	int iNombreArcs = SOMsommet.iSOMNb_entrant + SOMsommet.iSOMNb_sortant;
 	iGRANb_arcs =iGRANb_arcs - iNombreArcs;
@@ -47,14 +90,15 @@ void CGraphe::GRAsupprimer_sommet(CSommet SOMsommet)
 			delete[] pGRASommets[iSize];
 		}
 	}
-	
+
 
 
 }
 
-void CGraphe::GRAmodifier_sommet(int iNumero, CSommet SOMsommet)
+DANS LA MAUVAISE CLASSE ->> A DEPLACER*/
+
+
+int CGraphe::GRAlire_nb_arcs()
 {
-	int iDiffrence = pGRASommets[iNumero].iSOMNb_entrant + pGRASommets[iNumero].iSOMNb_sortant - SOMsommet.iSOMNb_entrant - SOMsommet.SOMlire_nb_sortant;
-	iGRANb_arcs = iGRANb_arcs - iDifference;
-	pGRASommets[iNumero] = SOMsommet;
+	return iGRANb_arcs;
 }
