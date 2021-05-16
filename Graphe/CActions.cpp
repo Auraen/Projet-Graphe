0a42d6ee2d278698cb270a442ecd46cfdedb7f16
Youssef696 <75251872+Youssef696@users.noreply.github.com>
2021-05-16 20:36:10 +0200

    AfiicherGraphe

--- Graphe/CActions.cpp
+++ Graphe/CActions.cpp
@@ -1,4 +1,7 @@
 #include "CActions.h"
+#include "CGraphe.h"
+#include "CArc.h"
+
 
 
 void CActions::ACTmodifier_arc(CArc ARCarc, int iNouvelle_dest)
@@ -6,5 +9,27 @@ void CActions::ACTmodifier_arc(CArc ARCarc, int iNouvelle_dest)
 	ARCarc.ARCmodifier_destination(iNouvelle_dest);
 }
 
+CGraphe ACTafficher_graphe(CGraphe GRAgraphe)
+{
+	int iBoucle, jBoucle;
+	cout << "Affige de grahe"<<endl;
+	for (iBoucle = 0; iBoucle < GRAgraphe.iGRANb_sommets ; iBoucle++)
+	{
+		cout << "sommet numéro:" << iBoucle;
+		cout << GRAgraphe.pGRASommets[iBoucle] << endl;
+		cout << "les prochains sommets  sont";
+		for (jBoucle = 0; jBoucle < GRAgraphe.pGRASommets[iBoucle].pSOMSortant->iSOMNb_sortant ; jBoucle++)
+		{
+			if (GRAgraphe.pGRASommets[iBoucle].pSOMSortant[jBoucle] != NULL) {
 
+				cout << GRAgraphe.pGRASommets[iBoucle].pSOMSortant[jBoucle]->iARCDestination << endl;
+			}
+			else {
+				cout << "pas d'arc sortant";
+			}
+		}
+	}
+	
+
+}
 
