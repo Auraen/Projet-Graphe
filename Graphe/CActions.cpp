8b4338cf1b8c2010cd44749b69f38148adc0307a
Youssef696 <75251872+Youssef696@users.noreply.github.com>
2021-05-17 17:49:32 +0200

    supprimer arc

--- Graphe/CActions.cpp
+++ Graphe/CActions.cpp
@@ -9,7 +9,7 @@ void CActions::ACTmodifier_arc(CArc ARCarc, int iNouvelle_dest)
 	ARCarc.ARCmodifier_destination(iNouvelle_dest);
 }
 
-/*CGraphe ACTafficher_graphe(CGraphe GRAgraphe)
+/*CGraphe CActions::ACTafficher_graphe(CGraphe GRAgraphe)
 {
 	
 	int iBoucle, jBoucle;
@@ -32,4 +32,42 @@ void CActions::ACTmodifier_arc(CArc ARCarc, int iNouvelle_dest)
 	}
 	
 }*/
+
+void CActions::ACTsupprimer_arc(CGraphe GRAgraphe, int iDepart, int iArrivee)
+{
+	int iBoucle,jBoucle;
+	//supprimer arc du sommet de depart
+	for (iBoucle = 0; iBoucle < GRAgraphe->iGRANb_sommets; iBoucle++) {
+		if (iDepart == GRAgraphe->pGRASommets[iBoucle]->iSOMNumero) {
+			for (jBoucle = 0; jBoucle < GRAgraphe->pGRASommets[iBoucle]->iSOMNb_sortant; jBoucle++) {
+				if (iArrivee == GRAgraphe->pGRASommets->pGRASommets[iBoucle]->pSOMSortant[jBoucle]->iARCDestination)
+				{
+					GRAgraphe->pGRASommets[iBoucle]->SOMsupprimer_arc_sortant(iArrivee);
+
+				}
+			}
+		}
+	}
+	
+
+	//supprimer arc du sommet de arrivee
+	for (iBoucle = 0; iBoucle < GRAgraphe->iGRANb_sommets; iBoucle++) {
+		if (iArrivee == GRAgraphe->pGRASommets[iBoucle]->iSOMNumero) {
+			for (jBoucle = 0; jBoucle < GRAgraphe->pGRASommets[iBoucle]->iSOMNb_entrant; jBoucle++) {
+				if (iArrivee == GRAgraphe->pGRASommets[iBoucle]->pSOMSortant[jBoucle]->iARCDestination)
+				{
+					GRAgraphe->pGRASommets->pGRASommets[iBoucle]->SOMsupprimer_arc_sortant(iArrivee);
+				}
+			}
+		}
+	}
+	
+}
+
+void CActions::ACTmodifier_arc(CGraphe GRAgraphe, CArc ARCarc, int iNouvelle_Dest)
+{
+
+}
+
+
 
